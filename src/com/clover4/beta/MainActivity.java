f7ced8517996cc5efa47f2819379cf35348986fe
forwchen <forwchen@gmail.com>
2014-02-06 20:31:26 +0800

    implements <get stdtable>

--- src/com/clover4/beta/MainActivity.java
+++ src/com/clover4/beta/MainActivity.java
@@ -1,14 +1,19 @@
 package com.clover4.beta;
 
 import java.io.File;
+import java.util.ArrayList;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 
 import com.clover4.utils.SharedprefHelper;
+
 
 
 import android.net.ConnectivityManager;
@@ -18,6 +23,7 @@ import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
@@ -42,8 +48,7 @@ public class MainActivity extends Activity {
 	private Button mLoginButton;
 	private String mpassword;
 	private String musrname;
-
-	
+	private SharedprefHelper mSharedprefHelper;
 	
 	public class LoginTask extends AsyncTask<Void, Void, Boolean>{
 
@@ -58,11 +63,13 @@ public class MainActivity extends Activity {
 			// TODO Auto-generated method stub
 			Boolean result = true;
 			try {
-				String LoginURL = "http://61.129.42.58:9083/school/login?user=13307130195&psw=6gjpq3a";
-				//String LoginURL = "http://61.129.42.58:9083/school/login?"+"user="+musrname+"&psw="+mpassword;
+				//String LoginURL = "http://61.129.42.58:9083/school/login?user=13307130195&psw=6gjpq3a";
+				String LoginURL = "http://61.129.42.58:9083/school/login?"+"user="+musrname+"&psw="+mpassword;
 				
 				HttpGet mHttpGet = new HttpGet(LoginURL);
 			    DefaultHttpClient mDefaultHttpClient = new DefaultHttpClient();
+
+			    
 				HttpResponse mHttpResponse = mDefaultHttpClient.execute(mHttpGet);
 			    if (mHttpResponse.getStatusLine().getStatusCode() == 200)
 			    {
@@ -86,7 +93,8 @@ public class MainActivity extends Activity {
 		@Override
 		protected void onPostExecute(Boolean result){
 			if (result == true) {
-				setContentView(R.layout.activity_main);
+				//setContentView(R.layout.activity_main);
+				doMain();
 			}
 		}
 		
@@ -100,6 +108,71 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
+	public class getStdTable extends AsyncTask<Void, Void, Boolean>{
+
+		@Override
+		protected Boolean doInBackground(Void... params) {
+			// TODO Auto-generated method stub
+
+			try {
+				HttpPost localHttpPost = new HttpPost("http://uis1.fudan.edu.cn/amserver/UI/Login");
+			
+				DefaultHttpClient localDefaultHttpClient = new DefaultHttpClient();
+			    localDefaultHttpClient.getParams().setParameter("http.protocol.allow-circular-redirects", Boolean.valueOf(true));
+			    ArrayList localArrayList = new ArrayList();
+			    localArrayList.add(new BasicNameValuePair("IDToken1", "13307130195"));
+			    localArrayList.add(new BasicNameValuePair("IDToken2", "6gjpq3a"));
+			    localHttpPost.setEntity(new UrlEncodedFormEntity(localArrayList, "UTF-8"));
+			    
+			    System.out.println("start");
+			    
+			    localDefaultHttpClient.execute(localHttpPost);
+			
+			    localHttpPost.abort();
+			    
+			    System.out.println("logged in");
+			    
+			    HttpGet localHttpGet = new HttpGet("http://xk.fudan.edu.cn/fdeams/stdCourseTable.action?xq=2013201402");
+			    HttpResponse localHttpResponse = localDefaultHttpClient.execute(localHttpGet);
+			    if (localHttpResponse.getStatusLine().getStatusCode() == 200)
+			    {
+			  
+			      String str = EntityUtils.toString(localHttpResponse.getEntity());
+			      System.out.println(str.replaceAll("\r", ""));
+
+			    }
+			    localHttpGet.abort();
+
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+	    
+		
+			return null;
+	
+		}
+	
+		protected void onPostExecute(Boolean result){
+		
+		}
+
+	
+}
+	
+	public void doMain(){
+		setContentView(R.layout.activity_main);
+		mSharedprefHelper.writeLong("logged_in", 1L);
+		Long stdTableStatus = mSharedprefHelper.readLong("table_downloaded");
+		if (stdTableStatus == 0){
+			getStdTable mgetStdTable = new getStdTable();
+			mgetStdTable.execute((Void)null);
+		}
+		else{
+			
+		}
+	}
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -111,10 +184,10 @@ public class MainActivity extends Activity {
 		}
 		else f.mkdir();
 		
-		SharedprefHelper mSharedprefHelper = new SharedprefHelper("setting",getApplicationContext());
-		Long logstatus = mSharedprefHelper.readLong("logged_in");
+		mSharedprefHelper = new SharedprefHelper("setting",getApplicationContext());
+		Long loginStatus = mSharedprefHelper.readLong("logged_in");
 		
-		if (logstatus == 0){
+		if (loginStatus == 0){
 			Log.d(TAG, SUCC);
 			/**
 			 * set login view
@@ -145,7 +218,7 @@ public class MainActivity extends Activity {
 							InputMethodManager  mInputMethodManager = (InputMethodManager)
 									MainActivity.this.getSystemService(Context.INPUT_METHOD_SERVICE);
 							mInputMethodManager.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS); 
-							
+		        
 							LoginTask mLoginTask = new LoginTask();
 							Boolean result = false;
 							try {
@@ -162,7 +235,8 @@ public class MainActivity extends Activity {
 			});
 		}
 		else {
-			setContentView(R.layout.activity_main);
+			//setContentView(R.layout.activity_main);
+			doMain();
 		}
 		/**
 		 * make app dir
