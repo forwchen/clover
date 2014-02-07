69be8cbeb758e91c28140994beeb941d76f779ca
forwchen <forwchen@gmail.com>
2014-02-07 13:09:50 +0800

    implements <loadtable>

---
+++ src/com/clover4/utils/TimeUtil.java
@@ -0,0 +1,30 @@
+package com.clover4.utils;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
+
+
+
+
+public class TimeUtil {
+	private SimpleDateFormat mSimpleDateFormat;
+	private Date mDate;
+	private Calendar mCalendar;
+	public String date;
+	public int dayofweek;
+	
+	
+	public TimeUtil() {
+		// TODO Auto-generated constructor stub
+		mSimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		mDate = new Date(System.currentTimeMillis());
+		date = mSimpleDateFormat.format(mDate);
+		
+		mCalendar = Calendar.getInstance();
+		mCalendar.setTimeZone(TimeZone.getTimeZone("GMT+8:00"));
+		dayofweek = mCalendar.DAY_OF_WEEK;
+		dayofweek = (dayofweek + 6) % 7;
+	}
+}
